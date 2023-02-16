03847beac5da62d47fd4c05787de063c2f5cc4f9
Victor Clivatti <joaoclivatti@hotmail.com>
2023-02-16 16:19:12 -0300

    Botucatu Citizens Update

--- agenda.cpp
+++ agenda.cpp
@@ -1,33 +1,46 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <iostream>
+#include <string>
+#include <cstdlib>
 
 using namespace std;
 
 struct pessoa{
-    char nome[10];
+    string nome;
     int ddd;
     int tel;
-    char cid[10];
+    string cid;
 };
 
 int main(){
 
-    struct pessoa p1[10];
+    struct pessoa p1[3];
     int i;
 
-    for(i=1;i<11;i++){
-        cout << "Digite o nome da pessoa " << i << ":";
+    for(i=0;i<3;i++){
+        cout << "Digite o nome da pessoa " << i+1 << ":";
         cin >> p1[i].nome;
+        cout << "\033[2J\033[1;1H";
 
-        cout << "Digite o DDD da pessoa " << i << ":";
+        cout << "Digite o DDD da pessoa " << i+1 << ":";
         cin >> p1[i].ddd;
+        cout << "\033[2J\033[1;1H";
 
-        cout << "Digite o telefone da pessoa " << i << ":";
+        cout << "Digite o telefone da pessoa " << i+1 << ":";
         cin >> p1[i].tel;
+        cout << "\033[2J\033[1;1H";
 
-        cout << "Digite a cidade da pessoa " << i << ":";
+        cout << "Digite a cidade da pessoa " << i+1 << ":";
         cin >> p1[i].cid;
+        cout << "\033[2J\033[1;1H";
+    }
+
+    for (i = 0; i < 3; i++) { // fixed the loop bounds
+        if (p1[i].cid == "Botucatu" || p1[i].cid == "botucatu") {
+            cout << "Dados da pessoa numero " << i+1 << ":" << endl; // added line break
+            cout << "Nome: " << p1[i].nome << endl; // added line break
+            cout << "Telefone: " << p1[i].ddd << p1[i].tel << endl; // added line break
+            cout << "Cidade: " << p1[i].cid << endl; // added line break
+        }
     }
 
     return 0;
